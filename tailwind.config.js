2e43f90cfe2118379fe653f6cd94c49b1788cf25
annaglova <anna.maliyenko@gmail.com>
2025-06-23 19:01:14 +0300

    Refactor project structure and migrate app to new directory
    - Deleted original index.html, vite.svg, App.css, App.tsx, index.css, main.tsx, vite-env.d.ts files.
    - Created new app structure under apps/app with updated index.html, App.css, App.tsx, index.css, main.tsx, and vite-env.d.ts.
    - Updated tailwind.config.js to reflect new content paths.
    - Adjusted tsconfig.app.json to include new app source directory.
    - Modified vite.config.ts to set the root and output directory for the new app.
    - Added landing directory with an empty index.tsx file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./apps/app/src/**/*.{js,ts,jsx,tsx}",
+    "./apps/landing/src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: "apps/app", // <-- це важливо!
   plugins: [react()],
-})
+  build: {
+    outDir: "../../dist/app",
+    emptyOutDir: true,
+  },
+});
