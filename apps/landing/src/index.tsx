bcc04fff40a91e25fa950fb26203dfd8e5ac64c4
annaglova <anna.maliyenko@gmail.com>
2025-06-24 15:26:25 +0300

    refactor: update entry point to main.tsx and remove old index.tsx files

--- apps/landing/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LandingRouter from "./router/LandingRouter";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <LandingRouter />
-  </React.StrictMode>
-);
