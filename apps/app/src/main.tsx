3da5e031ec71d892546e8d42131772f086b414e3
annaglova <anna.maliyenko@gmail.com>
2025-06-25 16:38:02 +0300

    refactor: replace Tailwind directives with CSS imports for base and theme styles

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../../../tailwind.base.css";
 import App from "./App";
 import "./index.css";
-import "./tailwind.theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
