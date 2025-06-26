d7fa42213533024d0af09e999db4b14e8480cb51
annaglova <anna.maliyenko@gmail.com>
2025-06-26 18:12:19 +0300

    feat: add custom theme styles for PrimeReact and update imports in CSS files

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,7 +1,7 @@
-import "primeicons/primeicons.css";
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import LandingRouter from "./router/LandingRouter";
 
 createRoot(document.getElementById("root")!).render(
